157e12bff94bb51f35fbfb0354d0b7404bf0fe76
Al-Atheer <moayadsu@gmail.com>
2024-11-08 12:01:15 +0100

    Moayad 1

--- menukarte.php
+++ menukarte.php
@@ -6,6 +6,7 @@
     <title>Spaghetti-Code</title>
     <!-- Verknüpfung zur externen CSS-Datei -->
     <link rel="stylesheet" href="./css/styles.css">
+    <link rel="stylesheet" href="./css/styles_menu.css">
 </head>
 <body>
     <!-- Navigationsleiste -->
@@ -19,7 +20,33 @@
         <a href="./impressum.php">Impressum</a>
     </nav>
 <main>
-    <h1>Menükarte</h1>
+    <h3>Menükarte</h3>
+    <div class="container">
+                <img src="Images\Classic Italian spaghetti meat balls.jpg" alt="">
+                <div class="overlay">
+                    <div class="text">Classic Italian<br>Spaghetti<br><br>13,60 €</div>
+                </div>
+            </div>
+            <div class="container">
+                <img src="Images\Homemade Spaghetti Sauce with fresh tomatoes.jpeg" alt="">
+                <div class="overlay">
+                        <div class="text">Homemade<br>SpaghettiSauce<br>with fresh<br><br>10,90 €</div>
+                </div>
+            </div>
+            <div class="container">
+                <img src="Images\Spaghetti Bolognese Recipe.jpg" alt="">
+                <div class="overlay">
+                    <div class="text">Spaghetti<br> Bolognese<br> Recipe<br><br><br></div>
+                    
+                    <div style="font-size: 15px; color:black" class="text">12,20 €</div>
+                </div>
+            </div>
+            <div class="container">
+                <img src="Images\The Best Spaghetti Salad.jpeg" alt="">
+                <div class="overlay">
+                    <div class="text">The Best<br>Spaghetti Salad<br><br>15,90 €</div>
+                </div>
+            </div>
 </main>
 
 </body>
